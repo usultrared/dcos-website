42cc8245db9dba82b1a0a97c4860f808d5db55c7
Karl Isenberg <karl@mesosphere.io>
2017-03-28 16:25:09 -0700

    Unicode workaround

--- ci/test.sh
+++ ci/test.sh
@@ -8,6 +8,10 @@ cd "${REPO_ROOT}"
 
 set -x
 
+# Workaround for https://github.com/dodo/node-unicodetable/issues/6
+# The unicode npm package fails to install on most OSes if unicode.org is blacklisting your IP.
+export NODE_UNICODETABLE_UNICODEDATA_TXT="$(pwd)/UnicodeData.txt"
+
 npm install
 npm rebuild node-sass
 CI=true npm test
